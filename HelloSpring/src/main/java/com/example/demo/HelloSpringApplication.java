b0474016aa594610b73c4b0e979b418b1f843113
AE205_PC3 <AE205_PC3@Infva06734>
2019-08-20 10:48:35 -0700

    nothing important

--- HelloSpring/src/main/java/com/example/demo/HelloSpringApplication.java
+++ HelloSpring/src/main/java/com/example/demo/HelloSpringApplication.java
@@ -2,14 +2,11 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 public class HelloSpringApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(HelloSpringApplication.class, args);
 	}
-
 }
